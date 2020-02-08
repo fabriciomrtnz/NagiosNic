b68e4e2a620cabef0e8f0444c2044370ffcfac81
Fabricio Martínez Tamayo <fabriciomrtnz@gmail.com>
2020-02-09 00:04:44 +0100

    Nagios check for the official speediest cli tool

---
+++ check_speedtest.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+
+import sys
+import pprint
+import argparse
+import commands
+import json
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-c', '--speedtest-cli', action='store', dest='cmd', required=True,
+                    help='Specify speedtest cli route.')
+
+parser.add_argument('-s', '--server', action='store', dest='server', required=True,
+                    help='Specify test server id.')
+
+parser.add_argument('-dw', action='store', dest='dwarn', type=int, required=True,
+                    help='Specify download warning.')
+
+parser.add_argument('-dc', action='store', dest='dcritic', type=int, required=True,
+                    help='Specify download critical.')
+
+parser.add_argument('-uw', action='store', dest='uwarn', type=int, required=True,
+                    help='Specify upload warning.')
+
+parser.add_argument('-uc', action='store', dest='ucritic', type=int, required=True,
+                    help='Specify upload critical.')
+
+parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+
+args = parser.parse_args()
+
+status, output = commands.getstatusoutput(args.cmd + " -s " + args.server + " -f json")
+data = json.loads(output)
+
+download = data["download"]["bandwidth"]
+upload = data["upload"]["bandwidth"]
+ping = data["ping"]["latency"]
+packetloss = data["packetLoss"]
+
+if int(download) > int(args.dcritic):
+    print("DOWNLOAD CRITICAL | DOWNLOAD=" + str(download) + " UPLOAD=" + str(upload) + " PING=" + str(ping) + " PACKETLOSS=" + str(packetloss))
+    exit(2)
+elif int(upload) > int(args.ucritic):
+    print("UPLOAD CRITICAL | DOWNLOAD=" + str(download) + " UPLOAD=" + str(upload) + " PING=" + str(ping) + " PACKETLOSS=" + str(packetloss))
+    exit(2)
+elif int(download) > int(args.dwarn):
+    print("DOWNLOAD WARNING | DOWNLOAD=" + str(download) + " UPLOAD=" + str(upload) + " PING=" + str(ping) + " PACKETLOSS=" + str(packetloss))
+    exit(1)
+elif int(upload) > int(args.uwarn):
+    print("UPLOAD WARNING | DOWNLOAD=" + str(download) + " UPLOAD=" + str(upload) + " PING=" + str(ping) + " PACKETLOSS=" + str(packetloss))
+    exit(1)
+else:
+    print("OK | DOWNLOAD=" + str(download) + " UPLOAD=" + str(upload) + " PING=" + str(ping) + " PACKETLOSS=" + str(packetloss))
+    exit(0)
