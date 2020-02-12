3195d4ad1eb98d2f056f27909151a1b385d539d0
Fabricio Martínez Tamayo <fabriciomrtnz@gmail.com>
2020-02-12 19:53:51 +0100

    Update check_speedtest.py

--- check_speedtest.py
+++ check_speedtest.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env python
-# Plugin that works with speedtes.net native cli
-# Needs to be setup as a passive check because it takes a while to run.
 
 import sys
 import pprint
 import argparse
 import subprocess
-import commands
 import json
 import os
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-c', '--speedtest-cli', action='store', dest='cmd', required=True,
-                    help='Specify speedtest cli route.')
+                            help='Specify speedtest cli route.')
 
 parser.add_argument('-s', '--server', action='store', dest='server', required=True,
-                    help='Specify test server id.')
+                            help='Specify test server id.')
 
 parser.add_argument('-dw', action='store', dest='dwarn', type=int, required=True,
-                    help='Specify download warning.')
+                            help='Specify download warning.')
 
 parser.add_argument('-dc', action='store', dest='dcritic', type=int, required=True,
-                    help='Specify download critical.')
+                            help='Specify download critical.')
 
 parser.add_argument('-uw', action='store', dest='uwarn', type=int, required=True,
-                    help='Specify upload warning.')
+                            help='Specify upload warning.')
 
 parser.add_argument('-uc', action='store', dest='ucritic', type=int, required=True,
-                    help='Specify upload critical.')
+                            help='Specify upload critical.')
 
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 
@@ -36,13 +33,13 @@ args = parser.parse_args()
 
 #status, output = commands.getstatusoutput(args.cmd + " -s " + args.server + " -f json")
 
-p = subprocess.Popen(args.cmd + " -s " + args.server + " -f json", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-(output, err) = p.communicate()
+cp = subprocess.Popen([args.cmd, '-s', args.server, '-f', 'json'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+(output, err) = cp.communicate()
 
 data = json.loads(output)
 
-download = data["download"]["bandwidth"]
-upload = data["upload"]["bandwidth"]
+download = int(data["download"]["bandwidth"]) / 131072
+upload = int(data["upload"]["bandwidth"]) / 131072
 ping = data["ping"]["latency"]
 packetloss = data["packetLoss"]
 
